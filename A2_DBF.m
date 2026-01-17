7fd2835e4ec60923d7f831b5579ede7c63a43b26
racheltipton9 <racheltipton9@gmail.com>
2026-01-17 13:52:30 -0500

    Add files via upload

--- A2_DBF.m
+++ A2_DBF.m
@@ -13,7 +13,6 @@ V_s = 10; % Stall Velocity [m/s]
 CL_max = 1.5; % Max Lift Coefficient
 
 wingloading_stall = 0.5 * rho * V_s^2 * CL_max;
-powerloading_stall = linspace(0,max_val,1000);
 
 %% Cruise Constraint
 eta_CR = 0.7; % Cruise Propellor Efficiency
@@ -27,9 +26,9 @@ powerloading_CR = ((eta_CR * phi) / (0.5 * rho * 1.1 * CD_0 * V_CR^3)) * wingloa
 eta_CL = 0.55; % Climb Propellor efficiency
 V_CL = 13; % Climb Velocity
 LD_max = 12;
-gamma = 20; % climb angle [deg]
+gamma = 10; % climb angle [deg]
 
-powerloading_CL = eta_CL / (V_CL * (1 / (0.866 * LD_max) + sind(gamma)));
+powerloading_CL = eta_CL / (V_CL * ( (1 / (0.866 * LD_max)) + sind(gamma)));
 
 %% Maneuver Constraint
 eta_M = 0.7;
@@ -39,18 +38,18 @@ e = 0.85; % Oswald Efficiency Factor
 n = 2; % Maneuvering load factor
 q = 0.5 * rho * V_M^2;
 
-wingloading_M = linspace(0,max_val,1000);
+wingloading_M = linspace(1e-8,max_val,1000);
 powerloading_M = eta_M ./ (q * V_M * (CD_0 ./ wingloading_M + 1/(pi * AR * e) * (n/q)^2 * wingloading_M));
 
 %% Takeoff Constraint
-CD_G = .01; % change!!!!!!!!
+CD_G = .0875; % Ground Drag Coefficient
 S_TO = 18; % TO run [m]
 mu = 0.04; % Ground friction coefficient
 CL_R = 2.1075; % CL at rotation
 eta_TO = 0.5; % TO prop efficiency
 V_TO = 12; % TO velocity [m/s]
 
-wingloading_TO = linspace(0,max_val,1000);
+wingloading_TO = linspace(1e-8,max_val,1000);
 exp_term = exp(0.6 .* rho .* g .* CD_G .* S_TO .* 1 ./ wingloading_TO);
 powerloading_TO = (eta_TO ./ V_TO) .* (1 - exp_term) ./ (mu - (mu + CD_G ./ CL_R) .* exp_term);
 
@@ -63,9 +62,10 @@ plot(wingloading_CR, powerloading_CR,"g");
 yline(powerloading_CL,"r");
 plot(wingloading_M, powerloading_M,"m")
 plot(wingloading_TO,powerloading_TO,"k")
-legend("stall","cruise","climb","maneuver","takeoff",Location="northwest")
+powerloading_stall_TO = interp1(wingloading_TO, powerloading_TO, wingloading_stall);
+plot(wingloading_stall,powerloading_stall_TO,'ko', 'MarkerFaceColor', "yellow", 'MarkerSize', 10)
+
+legend("Stall","Cruise","Climb","Maneuver","Takeoff","Design Point",Location="northwest")
 title("Aircraft Constraint Diagram")
 ylabel("Power Loading (W/P) [N/W]")
 xlabel("Wing Loading (W/S) [N/m^2]")
-
-

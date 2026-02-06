2b672e5c9feb5498fa9f7fe6071626afa5000ed7
Rachel Tipton <racheltipton9@gmail.com>
2026-02-06 12:36:18 -0500

    prop system requirements

--- prop_system.m
+++ prop_system.m
@@ -13,11 +13,16 @@ fprintf("Battery Power Required: %.4f [W]\n",P_req_bat);
 % calculating static thrust CLr = CLTo
 L_TO =  0.7*V_TO; % need to calculate this, this is an estimate
 D_TO = 0.7*V_TO; % need to calculate this
-T_0 = ((W_int*(V_TO^2))/(2*g*S_TO)) + D_TO + mu*(W_int - L_TO);
+T_0 = ((W_int*(V_TO^2))/(2*g*S_TO)) + D_TO + mu_ground*(W_int - L_TO);
 
 fprintf("Estimated Static Thrust: %.4f [N]\n",T_0);
 
 % calculating cruise thrust
 
-% T_cr = D_cr;
+CD_i = CL_CR^2 / (pi() * AR_wing * e);
 
+CD = CD_i + CD_0;
+D_cr = 0.5* CD*rho*(V_CR^2)*wing_area_total;
+T_cr = D_cr;
+
+fprintf("Thrust: %f [N]\n", T_cr)

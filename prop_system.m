867dada51f2c6c2e2d8eaaac546ac548b4889e8a
Kendall Roach <kendallroach13@gmail.com>
2026-02-11 13:06:29 -0500

    Added thrust values from chosen propeller to the prop system code and added thickness calc to the wing design

--- prop_system.m
+++ prop_system.m
@@ -14,8 +14,6 @@ fprintf("Battery Power Required: %.4f [W]\n",P_req_bat);
 % calculating static thrust CLr = CLTo
 L_TO =  0.7*V_TO; % estimate for static thrust
 D_TO = 0.7*V_TO; 
-L_TO =  0.7*V_TO; % need to calculate this, this is an estimate
-D_TO = 0.7*V_TO; % need to calculate this
 T_0 = ((W_int*(V_TO^2))/(2*g*S_TO)) + D_TO + mu_ground*(W_int - L_TO);
 
 fprintf("Estimated Static Thrust: %.4f [N]\n",T_0);
@@ -28,21 +26,92 @@ D_cr = 0.5*C_D*rho*(V_CR^2)*wing_area_total;
 T_cr = D_cr;
 
 % plotting thrust and drag vs airspeed
-velocity_array = linspace(15,60,1000);
-cl_array = (2*W_int)./(rho.*(velocity_array.^2).*wing_area_total);
+velocity_array = linspace(0,40,1000);
+% for loop for calculating the cl array at different velocities
+cl_array = zeros(size(velocity_array)); 
+for i = 1:length(velocity_array)
+    if velocity_array(i) <= V_TO % for before v takeoff make the value constant
+        cl_array(i) = CL_R;
+    else
+        cl_array(i) = (2 * W_int) / ...
+                      (rho * (velocity_array(i)^2) * wing_area_total); % calculating cl
+    end
+end
+
 cdi_array = (cl_array.^2)/(pi()*AR_wing*e);
 cd_array = CD_0 + cdi_array;
-Thrust_available = P_req_mech./velocity_array;
+% values from chosen prop system - 11X55E at 14,000 RPM
+velocity_fromprop = [0.000
+1.462
+2.928
+4.390
+5.856
+7.318
+8.780
+10.247
+11.709
+13.171
+14.638
+16.100
+17.566
+19.028
+20.490
+21.957
+23.419
+24.881
+26.348
+27.810
+29.276
+30.738
+32.200
+33.667
+35.129
+36.591
+38.058
+39.520
+40.987];
+
+thrust_fromprop = [40.981
+40.291
+39.549
+38.754
+37.903
+36.993
+36.023
+34.990
+33.894
+32.733
+31.507
+30.216
+28.859
+27.439
+25.955
+24.405
+22.797
+21.137
+19.431
+17.685
+15.904
+14.093
+12.258
+10.405
+8.541
+6.673
+4.811
+2.964
+1.143];
+
 D_array = 0.5*cd_array*rho.*((velocity_array).^2).*wing_area_total;
 
 figure();
-plot(velocity_array, Thrust_available, "m-", linewidth = 1.5);
+plot(velocity_fromprop, thrust_fromprop, "m-", linewidth = 1.5);
 hold on
 plot(velocity_array, D_array, "g-", linewidth = 1.5);
 title("Drag and Thrust vs Airspeed");
 ylabel("Forces (N)");
 xlabel('Airspeed (m/s)');
 legend('Thrust', 'Drag');
+xlim([0 40]);
 grid on;
 
 % plotting minimum power required vs airspeed
--- wing_design.m
+++ wing_design.m
@@ -7,4 +7,7 @@ wing_area_ind = wing_area_total / 2; % because of the biplane we need individual
 b = sqrt(AR_wing * wing_area_ind); % AR = b^2/A where b is span
 c = wing_area_ind / b; % chord
 
+wing_thickness_cm = 100* 0.117*c; % thickness is 11.7% of chord for the clark y
+
 fprintf("Wing Chord length: %.4f [m]\nWing Span: %.4f [m]\n",c,b)
+fprintf("Wing thickness: %.4f [cm]\n",wing_thickness_cm);
